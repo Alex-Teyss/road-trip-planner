d53bb93e6f634bdae95b3427fe5c3b1e70cda94b
Alex-Teyss <alextey28@gmail.com>
2024-03-15 18:17:15 +0100

    Add the correct config to run the server

--- back-end/main.py
+++ back-end/main.py
@@ -4,3 +4,9 @@ from config import app, db
 @app.route("/test", methods=["GET"])
 def test():
     return jsonify({"message": "Hello Flask"})
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+
+    app.run(debug=True)
